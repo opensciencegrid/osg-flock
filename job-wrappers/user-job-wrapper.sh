85815f4ad29c1ab713e1813a7860e1c319f77555
Mats Rynge <mats@rynge.net>
2015-12-08 19:31:41 -0800

    Enabled trace module

--- job-wrappers/user-job-wrapper.sh
+++ job-wrappers/user-job-wrapper.sh
@@ -64,6 +64,17 @@ fi
 
 #############################################################################
 #
+#  Trace callback
+#
+
+if [ ! -e ../../trace-callback ]; then
+    (wget -nv -O ../../trace-callback http://obelix.isi.edu/osg/agent/trace-callback && chmod 755 ../../trace-callback) >/dev/null 2>&1 || /bin/true
+fi
+../../trace-callback start >.trace-callback.log 2>&1 || /bin/true
+
+
+#############################################################################
+#
 #  fall through to next/default job wrapper
 #
 
