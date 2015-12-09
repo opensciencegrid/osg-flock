69bf414cd92a25cf141edb5b5032453ffa4c96b5
Mats Rynge <mats@rynge.net>
2015-12-08 19:05:43 -0800

    Trace fixes

--- job-wrappers/itb.sh
+++ job-wrappers/itb.sh
@@ -5,9 +5,9 @@
 #  Trace callback
 #
 
-if [ ! -e ../trace-callback ]; then
-    (wget -nv -O ../trace-callback http://osg-flock.grid.iu.edu/trace/trace-callback && chmod 755 ../trace-callback) >/dev/null 2>&1 || /bin/true
+if [ ! -e ../../trace-callback ]; then
+    (wget -nv -O ../../trace-callback http://obelix.isi.edu/osg/agent/trace-callback && chmod 755 ../../trace-callback) >/dev/null 2>&1 || /bin/true
 fi
-../trace-callback start >.trace-callback.log 2>&1 || /bin/true
+../../trace-callback start >.trace-callback.log 2>&1 || /bin/true
 
 
