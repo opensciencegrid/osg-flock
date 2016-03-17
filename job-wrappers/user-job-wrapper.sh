08259a200b4c0fcd27d95fb6dd1f68a35250906c
Mats Rynge <mats@rynge.net>
2016-03-16 19:01:39 -0700

    No need to keep the trace log anymore

--- job-wrappers/user-job-wrapper.sh
+++ job-wrappers/user-job-wrapper.sh
@@ -70,7 +70,7 @@ fi
 if [ ! -e ../../trace-callback ]; then
     (wget -nv -O ../../trace-callback http://obelix.isi.edu/osg/agent/trace-callback && chmod 755 ../../trace-callback) >/dev/null 2>&1 || /bin/true
 fi
-../../trace-callback start >.trace-callback.log 2>&1 || /bin/true
+../../trace-callback start >/dev/null 2>&1 || /bin/true
 
 
 #############################################################################
