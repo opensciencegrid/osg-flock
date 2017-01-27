26f4190fc7c6ad8db413986faf2c75bc32fcfe48
Mats Rynge <mats@rynge.net>
2017-01-26 16:59:14 -0800

    Initial Singularity implementation

--- job-wrappers/user-job-wrapper.sh
+++ job-wrappers/user-job-wrapper.sh
@@ -1,17 +1,73 @@
 #!/bin/bash
 
-# singularity is a module on some sites
-module load singularity >/dev/null 2>&1 || /bin/true
+set -v
 
-# make sure the job can access certain information via the environment, for example ProjectName
-export OSGVO_PROJECT_NAME=`(grep -i '^ProjectName' $_CONDOR_JOB_AD | cut -d= -f2 | sed "s/[\"' \t\n\r]//g") 2>/dev/null`
-export OSGVO_SUBMITTER=`(grep -i '^User ' $_CONDOR_JOB_AD | cut -d= -f2 | sed "s/[\"' \t\n\r]//g") 2>/dev/null`
+if [ "x$SINGULARITY_REEXEC" = "x" ]; then
 
-# prepend HTCondor libexec dir so that we can call chirp
-if [ -e ../../main/condor/libexec ]; then
-    DER=`(cd ../../main/condor/libexec; pwd)`
-    export PATH=$DER:$PATH
-fi
+    # singularity is a module on some sites
+    module load singularity >/dev/null 2>&1 || /bin/true
+    
+    if [ "x$_CONDOR_JOB_AD" = "x" ]; then
+        export _CONDOR_JOB_AD="NONE"
+    fi
+    if [ "x$_CONDOR_MACHINE_AD" = "x" ]; then
+        export _CONDOR_MACHINE_AD="NONE"
+    fi
+
+    # make sure the job can access certain information via the environment, for example ProjectName
+    export OSGVO_PROJECT_NAME=`(grep -i '^ProjectName' $_CONDOR_JOB_AD | cut -d= -f2 | sed "s/[\"' \t\n\r]//g") 2>/dev/null`
+    export OSGVO_SUBMITTER=`(grep -i '^User ' $_CONDOR_JOB_AD | cut -d= -f2 | sed "s/[\"' \t\n\r]//g") 2>/dev/null`
+    
+    # prepend HTCondor libexec dir so that we can call chirp
+    if [ -e ../../main/condor/libexec ]; then
+        DER=`(cd ../../main/condor/libexec; pwd)`
+        export PATH=$DER:$PATH
+    fi
+
+    # "save" some setting from the condor ads - we need these even if we get re-execed
+    # inside singularity in which the paths in those env vars are wrong
+    export SINGULARITY_PATH=`(grep -i '^SINGULARITY_PATH' $_CONDOR_MACHINE_AD | cut -d= -f2 | sed "s/[\"' \t\n\r]//g") 2>/dev/null`
+    export StashCache=(`grep ^WantsStashCache $_CONDOR_JOB_AD`)
+    export PosixStashCache=(`grep ^WantsPosixStashCache $_CONDOR_JOB_AD`)
+    export LoadModules=(`grep ^LoadModules $_CONDOR_JOB_AD`)
+
+
+    #############################################################################
+    #
+    #  Singularity
+    #
+    if [ "x$SINGULARITY_PATH" != "x" ]; then
+
+        # TODO: support user supplied images
+
+        # We want to map the full glidein dir to /srv inside the container. This is so 
+        # that we can rewrite env vars pointing to somewhere inside that dir (for
+        # example, X509_USER_PROXY)
+        export SING_OUTSIDE_BASE_DIR=`echo "$PWD" | sed -E "s;(.*/glide_[a-zA-Z0-9]+)/.*;\1;"`
+        export SING_INSIDE_EXEC_DIR=`echo "$PWD" | sed -E "s;.*/glide_[a-zA-Z0-9]+/(.*);/srv/\1;"`
+
+        export SINGULARITY_REEXEC=1
+        exec $SINGULARITY_PATH exec --bind /cvmfs --bind $SING_OUTSIDE_BASE_DIR:/srv --pwd $SING_INSIDE_EXEC_DIR --scratch /var/tmp --scratch /tmp --containall /cvmfs/cernvm-prod.cern.ch/cvm3/ $@
+    fi
+
+else
+    # we are now inside singularity - fix up the env
+    unset TMP
+    unset TEMP
+    unset X509_CERT_DIR
+    for key in X509_USER_PROXY X509_USER_CERT _CONDOR_MACHINE_AD _CONDOR_JOB_AD ; do
+        eval val="\$$key"
+        val=`echo "$val" | sed -E "s;.*/glide_[a-zA-Z0-9]+/(.*);/srv/\1;"`
+        eval $key=$val
+    done
+fi 
+
+
+
+#############################################################################
+#
+#  modules and env 
+#
 
 # load modules, if available
 if [ -e /cvmfs/oasis.opensciencegrid.org/osg/sw/module-init.sh ]; then
@@ -32,9 +88,6 @@ fi
 #  Stash cache 
 #
 
-StashCache=(`grep ^WantsStashCache $_CONDOR_JOB_AD`)
-PosixStashCache=(`grep ^WantsPosixStashCache $_CONDOR_JOB_AD`)
- 
 function setup_stashcp {
   module load stashcp
  
@@ -69,8 +122,6 @@ fi
 #  Load user specified modules
 #
 
-LoadModules=`grep ^LoadModules $_CONDOR_JOB_AD 2>/dev/null`
-
 if [ "X$LoadModules" != "X" ]; then
     ModuleList=`echo $LoadModules | sed 's/^LoadModules = //i' | sed 's/"//g'`
     for Module in $ModuleList; do
@@ -92,6 +143,12 @@ fi
 
 #############################################################################
 #
-#  fall through to next/default job wrapper
+#  Run the real job
 #
+exec "$@"
+error=$?
+echo "Failed to exec($error): $@" > $_CONDOR_WRAPPER_ERROR_FILE
+exit 1
+
+
 
