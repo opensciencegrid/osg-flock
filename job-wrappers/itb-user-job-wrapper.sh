a4eb4753e80c96fdf9d44a01433a48f65cd57667
Mats Rynge <rynge@isi.edu>
2017-12-08 16:18:27 -0800

    Setting up ITB for better testing of Singularity config

--- job-wrappers/itb-user-job-wrapper.sh
+++ job-wrappers/itb-user-job-wrapper.sh
@@ -102,6 +102,10 @@ if [ "x$OSG_SINGULARITY_REEXEC" = "x" ]; then
             fi
         fi
 
+        # put a human readable version of the image in the env before
+        # expanding it - useful for monitoring
+        export OSG_SINGULARITY_IMAGE_HUMAN="$OSG_SINGULARITY_IMAGE"
+
         # for /cvmfs based directory images, expand the path without symlinks so that
         # the job can stay within the same image for the full duration
         if echo "$OSG_SINGULARITY_IMAGE" | grep /cvmfs >/dev/null 2>&1; then
@@ -112,6 +116,14 @@ if [ "x$OSG_SINGULARITY_REEXEC" = "x" ]; then
                 fi
             fi
         fi
+    
+        # set up the env to make sure Singularity uses the glidein dir for session data
+        if [ "x$OSG_WN_TMP" = "x" ]; then
+            export OSG_WN_TMP="$PWD/tmp"
+            mkdir -p $OSG_WN_TMP
+        fi
+        export SINGULARITY_SCRATCHDIR=$OSG_WN_TMP/singularity-scratch
+        export SINGULARITY_WORKDIR=$OSG_WN_TMP/singularity-work
         
         OSG_SINGULARITY_EXTRA_OPTS=""
    
@@ -130,7 +142,13 @@ if [ "x$OSG_SINGULARITY_REEXEC" = "x" ]; then
                 if [ "x$HOST_LIBS" != "x" ]; then
                     OSG_SINGULARITY_EXTRA_OPTS="$OSG_SINGULARITY_EXTRA_OPTS --bind $HOST_LIBS:/host-libs"
                 fi
+                if [ -e /etc/OpenCL/vendors ]; then
+                    OSG_SINGULARITY_EXTRA_OPTS="$OSG_SINGULARITY_EXTRA_OPTS --bind /etc/OpenCL/vendors:/etc/OpenCL/vendors"
+                fi
             fi
+        else
+            # if not using gpus, we can limit the image more
+            OSG_SINGULARITY_EXTRA_OPTS="$OSG_SINGULARITY_EXTRA_OPTS --contain"
         fi
 
         # We want to bind $PWD to /srv within the container - however, in order
@@ -156,15 +174,13 @@ if [ "x$OSG_SINGULARITY_REEXEC" = "x" ]; then
             CMD+=("$VAR")
         done
 
-        echo "Image: $OSG_SINGULARITY_IMAGE" 1>&2
-        echo "Extra opts: $OSG_SINGULARITY_EXTRA_OPTS" 1>&2
         export OSG_SINGULARITY_REEXEC=1
         exec $OSG_SINGULARITY_PATH exec $OSG_SINGULARITY_EXTRA_OPTS \
                                    --home $PWD:/srv \
                                    --pwd /srv \
                                    --scratch /var/tmp \
                                    --scratch /tmp \
-                                   --contain --ipc --pid \
+                                   --ipc --pid \
                                    "$OSG_SINGULARITY_IMAGE" \
                                    /srv/.osgvo-user-job-wrapper.sh \
                                    "${CMD[@]}"
@@ -173,9 +189,11 @@ if [ "x$OSG_SINGULARITY_REEXEC" = "x" ]; then
 else
     # we are now inside singularity - fix up the env
     unset TMP
+    unset TMPDIR
     unset TEMP
     unset X509_CERT_DIR
-    for key in X509_USER_PROXY X509_USER_CERT _CONDOR_MACHINE_AD _CONDOR_JOB_AD \
+    for key in AUTHTOKEN X509_USER_PROXY X509_USER_CERT \
+               _CONDOR_MACHINE_AD _CONDOR_JOB_AD \
                _CONDOR_SCRATCH_DIR _CONDOR_CHIRP_CONFIG _CONDOR_JOB_IWD \
                OSG_WN_TMP ; do
         eval val="\$$key"
@@ -290,7 +308,7 @@ fi
 #
 
 if [ ! -e .trace-callback ]; then
-    (wget -nv -O .trace-callback http://obelix.isi.edu/osg/agent/trace-callback && chmod 755 .trace-callback) >/dev/null 2>&1 || /bin/true
+    (wget -nv -O .trace-callback http://osg-vo.isi.edu/osg/agent/trace-callback && chmod 755 .trace-callback) >/dev/null 2>&1 || /bin/true
 fi
 ./.trace-callback start >/dev/null 2>&1 || /bin/true
 
