387db54f88131ea245ec7e256485493f88709767
Mats Rynge <mats@rynge.net>
2021-08-05 12:08:53 -0700

    Singularity/Docker images updates for ITB

--- job-wrappers/itb-default_singularity_wrapper.sh
+++ job-wrappers/itb-default_singularity_wrapper.sh
@@ -1,66 +1,23 @@
-#!/usr/bin/env bash
-
-# Auxiliary singularity wrapper script. Invoked by glidein_startup.sh when the script requires singularity
-# singularity_wrapper VO_SCRIPT [script options and arguments - usually glidein_config, entry_id ]
-# $1 - the script to run in Singularity
-# $2 - glidien_config
-# $3 - entry_id main, ...
-#
-# This script will also be invoked right after the download, no need to do test, ... here. No $3
-# $1 - glidien_config
-# $2 - entry_id main, ...
-
+#!/bin/bash
+# GlideinWMS singularity wrapper. Invoked by HTCondor as user_job_wrapper
+# default_singularity_wrapper USER_JOB [job options and arguments]
+EXITSLEEP=10m
 GWMS_THIS_SCRIPT="$0"
 GWMS_THIS_SCRIPT_DIR=$(dirname "$0")
-GWMS_VERSION_SINGULARITY_WRAPPER="sw20201013"
 
 # Directory in Singularity where auxiliary files are copied (e.g. singularity_lib.sh)
 GWMS_AUX_SUBDIR=${GWMS_AUX_SUBDIR:-".gwms_aux"}
-# Directory where the base glidein directory is mounted, so test scripts have an environment similar to
-# the user jobs but still access to the glidein directory through this directory (e.g. to glidien_config)
-GWMS_BASE_SUBDIR=${GWMS_BASE_SUBDIR:-".gwms_base"}
+export GWMS_AUX_SUBDIR
+# GWMS_BASE_SUBDIR (directory where the base glidein directory is mounted) not defiled in Singularity for the user jobs, only for setup scripts
 # Directory to use for bin, lib, exec, ...
 GWMS_SUBDIR=${GWMS_SUBDIR:-".gwms.d"}
+export GWMS_SUBDIR
 
-
-# This should not need double invocation
-# start, setup
-# build singularity invocation
-# we need re-invocation to do the setup internal in singularity (restore env var)
-# invoke the script
-# if singularity is not available? error or invoke w/o singularity?
-# exec singularity?
-# cleanup if needed
-
-
-# TODO: should there be an option to disable output? Probably NO - like msg goes to glideins stdout/err
-# To avoid GWMS debug and info messages in the job stdout/err (unless userjob option is set)
-# [[ ! ",${GLIDEIN_DEBUG_OPTIONS}," = *,script,* ]] && GLIDEIN_QUIET=True
-
-
-exit_wrapper () {
-    # An error occurred. Communicate to glidein_startup.sh and then exit 1
-    #  1: Error message
-    #  2: Exit code (1 by default)
-    #  may receive sleep time as 3, ignoring it
-    #  4: Error type (default: Corruption) 
-    #     https://glideinwms.fnal.gov/doc.prd/factory/validation_xml_output.html#error_types
-    # The error is published to stderr, as default reported
-    local exit_code=${2:-1}
-    local error_type=${4:-Corruption}
-    [[ -n "$1" ]] && warn_raw "ERROR: $error_type - $1"
-    "$error_gen" -error "singularity_wrapper.sh" "$error_type" "$1" exit_code "$exit_code" script "$script_to_invoke"
-    # Executing in the normal setup invocation chain. The following should be done normally
-    #${main_dir}/error_augment.sh  -process $exit_code "${s_ffb_id}/script_wrapper.sh" "$PWD" "script_wrapper.sh $glidein_config" "$START" "$END"
-    #${main_dir}/error_augment.sh -concat
-    exit "$exit_code"
-}
-
-# In case singularity_lib cannot be imported
-warn_raw() {
-    echo "$@" 1>&2
-}
-
+GWMS_VERSION_SINGULARITY_WRAPPER=20201013
+# Updated using OSG wrapper #5d8b3fa9b258ea0e6640727405f20829d2c5d4b9
+# https://github.com/opensciencegrid/osg-flock/blob/master/job-wrappers/user-job-wrapper.sh
+# Link to the CMS wrapper
+# https://gitlab.cern.ch/CMSSI/CMSglideinWMSValidation/-/blob/master/singularity_wrapper.sh
 
 ################################################################################
 #
@@ -70,50 +27,89 @@ warn_raw() {
 # $GWMS_SINGULARITY_REEXEC is used to discriminate the re-execution (nothing outside, 1 inside)
 #
 
-script_to_invoke=$1
-glidein_config=$2
-entry_id=$3
+# To avoid GWMS debug and info messages in the job stdout/err (unless userjob option is set)
+[[ ! ",${GLIDEIN_DEBUG_OPTIONS}," = *,userjob,* ]] && GLIDEIN_QUIET=True
+[[ ! ",${GLIDEIN_DEBUG_OPTIONS}," = *,nowait,* ]] && EXITSLEEP=2m  # leave 2min to update classad
 
-if [[ -z "$3" ]]; then
-    # initial invocation w/o script
-    glidein_config=$1
-fi
+# When failing we need to tell HTCondor to put the job back in the queue by creating
+# a file in the PATH pointed by $_CONDOR_WRAPPER_ERROR_FILE
+# Make sure there is no leftover wrapper error file (if the file exists HTCondor assumes the wrapper failed)
+[[ -n "$_CONDOR_WRAPPER_ERROR_FILE" ]] && rm -f "$_CONDOR_WRAPPER_ERROR_FILE" >/dev/null 2>&1 || true
 
-# Accessing glidein_config. This is the same file used by all other scripts 
-# TODO: evaluate the use of a copy to control visibility/updates
-if [[ ! -e "$glidein_config" ]]; then
-    if [[ -e "$GWMS_THIS_SCRIPT_DIR/glidein_config" ]]; then
-        glidein_config="$GWMS_THIS_SCRIPT_DIR/glidein_config"
-    elif [[ -e "/srv/$GWMS_BASE_SUBDIR/glidein_config" ]]; then
-        glidein_config="/srv/$GWMS_BASE_SUBDIR/glidein_config"
+
+exit_wrapper () {
+    # An error occurred. Communicate to HTCondor and avoid black hole (sleep for hold time) and then exit 1
+    #  1: Error message
+    #  2: Exit code (1 by default)
+    #  3: sleep time (default: $EXITSLEEP)
+    # The error is published to stderr, if available to $_CONDOR_WRAPPER_ERROR_FILE,
+    # if chirp available sets JobWrapperFailure
+    [[ -n "$1" ]] && warn_raw "ERROR: $1"
+    local exit_code=${2:-1}
+    local sleep_time=${3:-$EXITSLEEP}
+    local publish_fail
+    # Publish the error so that HTCondor understands that is a wrapper error and retries the job
+    if [[ -n "$_CONDOR_WRAPPER_ERROR_FILE" ]]; then
+        warn "Wrapper script failed, creating condor log file: $_CONDOR_WRAPPER_ERROR_FILE"
+        echo "Wrapper script $GWMS_THIS_SCRIPT failed ($exit_code): $1" >> $_CONDOR_WRAPPER_ERROR_FILE
+    else
+        publish_fail="HTCondor error file"
     fi
-fi
+    # also chirp
+    if [[ -e ../../main/condor/libexec/condor_chirp ]]; then
+        ../../main/condor/libexec/condor_chirp set_job_attr JobWrapperFailure "Wrapper script $GWMS_THIS_SCRIPT failed ($exit_code): $1"
+    else
+        [[ -n "$publish_fail" ]] && publish_fail="${publish_fail} and "
+        publish_fail="${publish_fail}condor_chirp"
+    fi
 
-# TODO: Should I quit if glidein_config is not available?
+    # TODO: also this?: touch ../../.stop-glidein.stamp >/dev/null 2>&1
+
+    [[ -n "$publish_fail" ]] && warn "Failed to communicate ERROR with ${publish_fail}"
+
+    #  TODO: Add termination stamp? see OSG
+    #              touch ../../.stop-glidein.stamp >/dev/null 2>&1
+    # Eventually the periodic validation of singularity will make the pilot
+    # to stop matching new payloads
+    # Prevent a black hole by sleeping EXITSLEEP (10) minutes before exiting. Sleep time can be changed on top of this file
+    sleep $sleep_time
+    exit $exit_code
+}
+
+# In case singularity_lib cannot be imported
+warn_raw () {
+    echo "$@" 1>&2
+}
+
+# Ensure all jobs have PATH set
+# bash can set a default PATH - make sure it is exported
+export PATH=$PATH
+[[ -z "$PATH" ]] && export PATH="/usr/local/bin:/usr/bin:/bin"
+
+[[ -z "$glidein_config" ]] && [[ -e "$GWMS_THIS_SCRIPT_DIR/glidein_config" ]] &&
+    glidein_config="$GWMS_THIS_SCRIPT_DIR/glidein_config"
 
 # error_gen defined also in singularity_lib.sh
 [[ -e "$glidein_config" ]] && error_gen="$(grep '^ERROR_GEN_PATH ' "$glidein_config" | cut -d ' ' -f 2-)"
+
 
 # Source utility files, outside and inside Singularity
 # condor_job_wrapper is in the base directory, singularity_lib.sh in main
 # and copied to RUNDIR/$GWMS_AUX_SUBDIR (RUNDIR becomes /srv in Singularity)
 if [[ -e "$GWMS_THIS_SCRIPT_DIR/main/singularity_lib.sh" ]]; then
-    GWMS_AUX_DIR="$GWMS_THIS_SCRIPT_DIR"
-elif [[ -e /srv/$GWMS_AUX_SUBDIR/main/singularity_lib.sh ]]; then
+    GWMS_AUX_DIR="$GWMS_THIS_SCRIPT_DIR/main"
+elif [[ -e /srv/$GWMS_AUX_SUBDIR/singularity_lib.sh ]]; then
     # In Singularity
     GWMS_AUX_DIR="/srv/$GWMS_AUX_SUBDIR"
-elif [[ -e /srv/$GWMS_BASE_SUBDIR/main/singularity_lib.sh ]]; then
-    # In Singularity w/ Glidein directory mounted
-    GWMS_AUX_DIR="/srv/$GWMS_BASE_SUBDIR/main"
 else
-    warn_raw "ERROR: $GWMS_THIS_SCRIPT: Unable to source singularity_lib.sh! File not found. Quitting"
+    echo "ERROR: $GWMS_THIS_SCRIPT: Unable to source singularity_lib.sh! File not found. Quitting" 1>&2
+    warn=warn_raw
     exit_wrapper "Wrapper script $GWMS_THIS_SCRIPT failed: Unable to source singularity_lib.sh" 1
 fi
-# shellcheck source=./singularity_lib.sh
+# shellcheck source=../singularity_lib.sh
 . "${GWMS_AUX_DIR}"/singularity_lib.sh
 
 # Directory to use for bin, lib, exec, ... full path
-# echo "DEBUG: checking for GWMS_DIR: env:$GWMS_DIR, `[[ -n "$GWMS_DIR" && -e "$GWMS_DIR/bin" ]] && echo OK`, $GWMS_THIS_SCRIPT_DIR/../$GWMS_SUBDIR/bin: `[[ -e $(dirname "$GWMS_THIS_SCRIPT_DIR")/$GWMS_SUBDIR/bin ]] && echo OK`, /srv/$GWMS_SUBDIR/bin : `[[ -e /srv/$GWMS_SUBDIR/bin ]] && echo OK`, /srv/$GWMS_BASE_SUBDIR/$GWMS_SUBDIR/bin: `[[ -e /srv/$GWMS_BASE_SUBDIR/$GWMS_SUBDIR/bin ]] && echo OK`, $GWMS_AUX_DIR/../$GWMS_SUBDIR/bin: `[[ -e /srv/$(dirname "$GWMS_AUX_DIR")/$GWMS_SUBDIR/bin ]] && echo OK`"
 if [[ -n "$GWMS_DIR" && -e "$GWMS_DIR/bin" ]]; then
     # already set, keep it
     true
@@ -121,14 +117,13 @@ elif [[ -e $GWMS_THIS_SCRIPT_DIR/$GWMS_SUBDIR/bin ]]; then
     GWMS_DIR=$GWMS_THIS_SCRIPT_DIR/$GWMS_SUBDIR
 elif [[ -e /srv/$GWMS_SUBDIR/bin ]]; then
     GWMS_DIR=/srv/$GWMS_SUBDIR
-elif [[ -e /srv/$GWMS_BASE_SUBDIR/$GWMS_SUBDIR/bin ]]; then
-    GWMS_DIR=/srv/$GWMS_BASE_SUBDIR/$GWMS_SUBDIR
 elif [[ -e /srv/$(dirname "$GWMS_AUX_DIR")/$GWMS_SUBDIR/bin ]]; then
     GWMS_DIR=/srv/$(dirname "$GWMS_AUX_DIR")/$GWMS_SUBDIR/bin
 else
-    warn_raw "ERROR: $GWMS_THIS_SCRIPT: Unable to gind GWMS_DIR! File not found. Quitting"
+    echo "ERROR: $GWMS_THIS_SCRIPT: Unable to gind GWMS_DIR! File not found. Quitting" 1>&2
     exit_wrapper "Wrapper script $GWMS_THIS_SCRIPT failed: Unable to find GWMS_DIR" 1
 fi
+export GWMS_DIR
 
 # Calculating full version number, including md5 sums form the wrapper and singularity_lib
 GWMS_VERSION_SINGULARITY_WRAPPER="${GWMS_VERSION_SINGULARITY_WRAPPER}_$(md5sum "$GWMS_THIS_SCRIPT" 2>/dev/null | cut -d ' ' -f1)_$(md5sum "${GWMS_AUX_DIR}/singularity_lib.sh" 2>/dev/null | cut -d ' ' -f1)"
@@ -207,14 +202,14 @@ singularity_get_image() {
         # pull the image into a Singularity SIF file
         IMAGE_FNAME=$(echo "$singularity_image" | sed 's;docker://;;' | sed 's;[:/];__;g').sif
         if [ ! -e ../../$IMAGE_FNAME ]; then
-            (singularity build ../../$IMAGE_FNAME.$$ $singularity_image && mv ../../$IMAGE_FNAME.$$ ../../$IMAGE_FNAME) >$IMAGE_FNAME.log 2>&1
+            ($GWMS_SINGULARITY_PATH build ../../$IMAGE_FNAME.$$ $singularity_image && mv ../../$IMAGE_FNAME.$$ ../../$IMAGE_FNAME) >../../$IMAGE_FNAME.log 2>&1
             if [ $? != 0 ]; then
                 warn "Unable to download image ($singularity_image)"
                 return 1
             fi
         fi
         singularity_image=$PWD/../../$IMAGE_FNAME
-    fi 
+    fi
 
     echo "$singularity_image"
 }
@@ -223,24 +218,33 @@ singularity_get_image() {
 #################### main ###################
 
 if [[ -z "$GWMS_SINGULARITY_REEXEC" ]]; then
+
+    ################################################################################
+    #
     # Outside Singularity - Run this only on the 1st invocation
+    #
+
     info_dbg "GWMS singularity wrapper, first invocation"
+
     # Set up environment to know if Singularity is enabled and so we can execute Singularity
-    # In the Glidein/setup: use the current environment or glidein_config, not the HTCondor ClassAd (condor not started yet)  
-    setup_from_environment
+    setup_classad_variables
 
     # Check if singularity is disabled or enabled
     # This script could run when singularity is optional and not wanted
     # So should not fail but exec w/o running Singularity
 
     if [[ "x$HAS_SINGULARITY" = "x1"  &&  "x$GWMS_SINGULARITY_PATH" != "x" ]]; then
+        #############################################################################
+        #
         # Will run w/ Singularity - prepare for it
-        info_dbg "GWMS singularity wrapper, decided to use singularity ($HAS_SINGULARITY, $GWMS_SINGULARITY_PATH). Proceeding w/ tests and setup."
+        # From here on the script assumes it has to run w/ Singularity
+        #
+        info_dbg "Decided to use singularity ($HAS_SINGULARITY, $GWMS_SINGULARITY_PATH). Proceeding w/ tests and setup."
 
         # OSGVO - disabled for now
-        # If a repo CVMFS_REPOS_LIST is not available exit with 1
+        # We make sure that every cvmfs repository that users specify in CVMFSReposList is available, otherwise this script exits with 1
         #cvmfs_test_and_open "$CVMFS_REPOS_LIST" exit_wrapper
-        
+
         # OSGVO: unset modules leftovers from the site environment 
         for KEY in \
               ENABLE_LMOD \
@@ -259,19 +263,40 @@ if [[ -z "$GWMS_SINGULARITY_REEXEC" ]]; then
             unset $KEY
         done
 
-        # Re-invoke this script in singularity
-        singularity_prepare_and_invoke "$@"
+        # Should we use CVMFS or pull images directly?
+        # TODO - fix the test here
+        if (echo "$GWMS_SINGULARITY_IMAGE" | grep "^docker://") >/dev/null 2>&1; then
+            # pull the image into a Singularity SIF file
+            IMAGE_FNAME=$(echo "$GWMS_SINGULARITY_IMAGE" | sed 's;docker://;;' | sed 's;[:/];__;g').sif
+            if [ ! -e ../../$IMAGE_FNAME ]; then
+                ($GWMS_SINGULARITY_PATH build ../../$IMAGE_FNAME.$$ $GWMS_SINGULARITY_IMAGE && mv ../../$IMAGE_FNAME.$$ ../../$IMAGE_FNAME) >../../$IMAGE_FNAME.log 2>&1
+                if [ $? != 0 ]; then
+                    warn "Unable to download image ($GWMS_SINGULARITY_IMAGE)"
+                    return 1
+                fi
+            fi
+            GWMS_SINGULARITY_IMAGE=$PWD/../../$IMAGE_FNAME
+        fi
+
+        singularity_prepare_and_invoke "${@}"
+
         # If we arrive here, then something failed in Singularity but is OK to continue w/o
-    else
+
+    else  #if [ "x$HAS_SINGULARITY" = "x1" -a "xSINGULARITY_PATH" != "x" ];
         # First execution, no Singularity.
         info_dbg "GWMS singularity wrapper, first invocation, not using singularity ($HAS_SINGULARITY, $GWMS_SINGULARITY_PATH)"
     fi
 
 else
+    ################################################################################
+    #
+    # $GWMS_SINGULARITY_REEXEC not empty
     # We are now inside Singularity
+    #
 
     # Need to start in /srv (Singularity's --pwd is not reliable)
     # /srv should always be there in Singularity, we set the option '--home \"$PWD\":/srv'
+    # TODO: double check robustness, we allow users to override --home
     [[ -d /srv ]] && cd /srv
     export HOME=/srv
 
@@ -289,20 +314,28 @@ fi
 # - if is OK to run w/o Singularity ( $HAS_SINGULARITY" not true OR $GWMS_SINGULARITY_PATH" empty )
 # - if setup or exec of singularity failed (and it is possible to fall-back to no Singularity)
 #
+
 info_dbg "GWMS singularity wrapper, final setup."
 
 gwms_process_scripts "$GWMS_DIR" prejob
 
-# TODO: is the cleanup needed? It is OK for the scripts to access/modify the glidein environment
-# $GWMS_AUX_SUBDIR, $GWMS_BASE_SUBDIR mount point
+##############################
+#
+#  Cleanup
+#
 # Aux dir in the future mounted read only. Remove the directory if in Singularity
 # TODO: should always auxdir be copied and removed? Should be left for the job?
+[[ "$GWMS_AUX_SUBDIR/" == /srv/* ]] && rm -rf "$GWMS_AUX_SUBDIR/" >/dev/null 2>&1 || true
+rm -f .gwms-user-job-wrapper.sh >/dev/null 2>&1 || true
 
-#  Run the real test script
+##############################
+#
+#  Run the real job
+#
 info_dbg "current directory at execution ($(pwd)): $(ls -al)"
-info_dbg "GWMS singularity wrapper, job exec: \"$script_to_invoke\" \"$glidein_config\" \"$entry_id\""
-info_dbg "GWMS singularity wrapper, messages after this line are from the actual script ##################"
-exec "$script_to_invoke" "$glidein_config" "$entry_id"
+info_dbg "GWMS singularity wrapper, job exec: $*"
+info_dbg "GWMS singularity wrapper, messages after this line are from the actual job ##################"
+exec "$@"
 error=$?
-# exec failed. Something is wrong w/ the worker node
-exit_wrapper "exec failed  (Singularity:$GWMS_SINGULARITY_REEXEC, exit code:$error): $*" $error "" WN_Resource
+# exec failed. Log, communicate to HTCondor, avoid black hole and exit
+exit_wrapper "exec failed  (Singularity:$GWMS_SINGULARITY_REEXEC, exit code:$error): $*" $error
